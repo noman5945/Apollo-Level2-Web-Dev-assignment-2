5da19f450b738ce260fa59390930137a69ec9f90
noman5945 <abdullahnoman.5945@gmail.com>
2024-06-01 11:49:10 +0600

    [SOLVED]API routes checked and Redeployed

--- src/app/modules/product/product.routes.ts
+++ src/app/modules/product/product.routes.ts
@@ -1,18 +1,18 @@
-import express from 'express'
-import { ProductControllers } from './product.controller'
+import express from "express";
+import { ProductControllers } from "./product.controller";
 
-const routes=express.Router()
+const routes = express.Router();
 
-routes.post('/create-product',ProductControllers.createNewProduct)
+routes.post("/", ProductControllers.createNewProduct);
 
-routes.get('/',ProductControllers.getAllProducts)
+routes.get("/", ProductControllers.getAllProducts);
 
-routes.get('/:productId',ProductControllers.getProductByID)
+routes.get("/:productId", ProductControllers.getProductByID);
 
-routes.put('/:productId',ProductControllers.upsertProductByID)
+routes.put("/:productId", ProductControllers.upsertProductByID);
 
-routes.delete('/:productId',ProductControllers.deleteProductByID)
+routes.delete("/:productId", ProductControllers.deleteProductByID);
 
-routes.get('/search/',ProductControllers.searchProductByKeyword)
+routes.get("/search/", ProductControllers.searchProductByKeyword);
 
-export const ProductRoutes=routes
+export const ProductRoutes = routes;
